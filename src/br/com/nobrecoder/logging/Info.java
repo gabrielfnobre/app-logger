c561dfb78694bc1460cac96d0eb842ba0776b044
Gabriel <nobrecoder@gmail.com>
2024-06-09 08:32:53 -0300

    09/06/2024

--- src/br/com/nobrecoder/logging/Info.java
+++ src/br/com/nobrecoder/logging/Info.java
@@ -3,10 +3,13 @@ package br.com.nobrecoder.logging;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/* Esta classe possuí um método que captura a data atual e retorna uma string com data e hora!
+ * */
+
 public class Info {
 
 	public static String showInfo() {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		Date currentDate = new Date();
 		String infoDate = dateFormat.format(currentDate); 
 		return infoDate;
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,3 @@
-module app.logger {
-	exports br.com.nobrecoder.logging;
+module app.logger { //A palavra reservada "module" nos permite dar um nome ao módulo do nosso projeto, e além disso serve para abrirmos o nosso bloco de módulos, para exportar e receber módulos de terceiros!
+	exports br.com.nobrecoder.logging; //Aqui estamos exportando o nosso pacote "logging" para quem desejar utilizá-lo!
 }
